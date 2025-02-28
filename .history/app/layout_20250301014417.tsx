d11a232c7475926b7c5278be8872ca274b61e59c
duresaguye <dureguye2@gmail.com>
2025-03-01 01:45:01 +0300

    inter resolved

--- .history/app/layout_20250301014417.tsx
+++ .history/app/layout_20250301014417.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-         className={inter.className}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
