bc562ac38da76435a38efff01dc804618beec16f
duresaguye <dureguye2@gmail.com>
2025-03-01 01:51:00 +0300

    changed

--- .history/tailwind.config.temp_20250228233508.js
+++
@@ -1,7 +0,0 @@
-// filepath: /home/dura/web/digital/digital-note/tailwind.config.temp.js
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./components/**/*.{js,ts,jsx,tsx,mdx}"], // You can adjust the content paths
-    plugins: [require("@tailwindcss/typography")],
-    corePlugins: false, // Disable core plugins to only get typography styles
-  };
